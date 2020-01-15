fa1013a325398b47add176404a66c9a6d289c0b0
jakubbachniak <jj.bachniak@gmail.com>
2020-01-15 09:37:45 +0000

    product alert notification button event working.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,9 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  onNotify() {
+    window.alert("This is an event handler for the notify button.");
+  }
 }
 
 
